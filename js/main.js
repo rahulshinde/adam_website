25601d3f618b5b8df858b0165e0407edc6e4fa12
Rahul Shinde <rshinde@mica.edu>
2017-06-06 20:56:21 -0400

    adding stuff, idgaf rn

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@ $(document).ready( function(){
   $(document).on('scroll', Site.toggleNav);
 
   $('.nav_link').on('click', Site.scrollToSection);
-  $('img').on('click', Site.openImage);
+  $('.project_image').on('click', Site.openImage);
+  $('#close').on('click', Site.closeImage);
+
+  $('#left').on('click', Site.prevImage);
+  $('#right').on('click', Site.nextImage);
+
+  $('#about').on('click', Site.openAbout);
 
   Site.toggleNav();
 })
@@ -48,7 +54,27 @@ Site.toggleNav = function(){
   } 
 }
 
+Site.openAbout = function(){
+  $('#about_modal').fadeToggle('slow');
+}
+
 Site.openImage = function(){
+  
+  Site.current = $(this).index() + 1;
+
+  $('body').addClass('stopscroll');
+
+  Site.slideshow_array = $(this).parent().find($('img'));
+  Site.slideshow_length = Site.slideshow_array.length;
+
+  console.log(Site.current);
+
+  $('#total').empty();
+  $('#total').append(Site.slideshow_length);
+
+  $('#current').empty();
+  $('#current').append(Site.current);
+
   $('.image_modal_container').css('background', 'url(' + $(this).data('src') + ')');
   $('.image_caption').empty();
   $('.image_caption').append($(this).data('text'));
@@ -56,6 +82,64 @@ Site.openImage = function(){
   $('.image_modal_box').toggle(); 
   $('.image_modal_container').toggle();
   $('.image_caption').toggle();
+}
 
+Site.closeImage = function(){
+  console.log('hello');
+  $('.image_modal_box').css('display', 'none'); 
+  $('.image_modal_container').css('display', 'none');
+  $('.image_caption').css('display', 'none');
+  $('body').removeClass('stopscroll');
+}
 
+Site.prevImage = function(){
+  if (Site.current == 1){
+    Site.current = Site.slideshow_length;
+
+    $('#current').empty();
+    $('#current').append(Site.current);
+
+    Site.current_image = Site.slideshow_array[Site.slideshow_length - 1];
+
+    $('.image_modal_container').css('background', 'url(' + $(Site.current_image).data('src') + ')');
+    $('.image_caption').empty();
+    $('.image_caption').append($(Site.current_image).data('text'));
+  } else{
+    Site.current = Site.current - 1;
+
+    $('#current').empty();
+    $('#current').append(Site.current);
+
+    Site.current_image = Site.slideshow_array[Site.current - 1];
+
+    $('.image_modal_container').css('background', 'url(' + $(Site.current_image).data('src') + ')');
+    $('.image_caption').empty();
+    $('.image_caption').append($(Site.current_image).data('text'));
+  }
+}
+
+Site.nextImage = function(){
+  if (Site.current == Site.slideshow_length){
+    Site.current = 1;
+
+    $('#current').empty();
+    $('#current').append(Site.current);
+
+    Site.current_image = Site.slideshow_array[0];
+
+    $('.image_modal_container').css('background', 'url(' + $(Site.current_image).data('src') + ')');
+    $('.image_caption').empty();
+    $('.image_caption').append($(Site.current_image).data('text'));
+  } else{
+    Site.current = Site.current + 1;
+
+    $('#current').empty();
+    $('#current').append(Site.current);
+
+    Site.current_image = Site.slideshow_array[Site.current - 1];
+
+    $('.image_modal_container').css('background', 'url(' + $(Site.current_image).data('src') + ')');
+    $('.image_caption').empty();
+    $('.image_caption').append($(Site.current_image).data('text'));
+  }
 }
